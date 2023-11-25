809945617c185fae90e3cc8245a64928be71a18e
LinThit27 <linthit2745@gmail.com>
2023-11-26 00:04:48 +0630

    fix create browser router,nav bar not appearing in  help link

--- src/layout/Help.jsx
+++ src/layout/Help.jsx
@@ -1,23 +1,38 @@
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Help() {
-    return (
-    <div>    
-        <header>
-            <nav className="px-20 my-5">
-                    <h1 className="text-2xl text-white text-start font-bold">Website Help Center</h1>
-                    <p className="text-white font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni praesentium dolore officiis
-                        perspiciatis illum inventore, quas alias temporibus sed aspernatur.</p>
-                <div className="flex justify-center gap-5 mt-5">
-                    <NavLink to="faq" className="px-2 py-1 border-2 border-red-500 rounded-md text-red-500 hover:bg-red-500 hover:border-white hover:text-white">FAQ</NavLink>
-                    <NavLink to="contact" className="px-2 py-1 border-2 border-red-500 rounded-md text-red-500 hover:bg-red-500 hover:border-white hover:text-white">Contact Us</NavLink>
-                </div>
-            </nav>
-        </header>
-        
-        <main>
-            <Outlet/>
-        </main>
+  return (
+    <div>
+      <header>
+        <nav className="px-20 my-10">
+          <h1 className="text-2xl  text-white text-start font-bold">
+            Website Help Center
+          </h1>
+          <p className="text-white font-light">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+            praesentium dolore officiis perspiciatis illum inventore, quas alias
+            temporibus sed aspernatur.
+          </p>
+          <div className="flex justify-center gap-5 mt-5">
+            <NavLink
+              to="faq"
+              className="px-2 py-1 border-2 border-red-500 rounded-md text-red-500 hover:bg-red-500 hover:border-white hover:text-white"
+            >
+              FAQ
+            </NavLink>
+            <NavLink
+              to="contact"
+              className="px-2 py-1 border-2 border-red-500 rounded-md text-red-500 hover:bg-red-500 hover:border-white hover:text-white"
+            >
+              Contact Us
+            </NavLink>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
+  );
 }

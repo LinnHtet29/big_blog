f3a7bf5175c779ba1482b3c11b500f15ee072923
LinThit27 <linthit2745@gmail.com>
2023-11-25 19:42:17 +0630

    first commit for login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  console.log("login");
+  return (
+    <div className="h-screen">
+      <h2 className="text-3xl font-extrabold">Loggin</h2>
+    </div>
+  );
+};
+
+export default Login;

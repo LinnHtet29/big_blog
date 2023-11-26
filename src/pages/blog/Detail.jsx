542706c285641fe2b51e53596aa7eadc9ea30561
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2023-11-26 09:42:05 +0630

    Nothing Changes

--- src/pages/blog/Detail.jsx
+++ src/pages/blog/Detail.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export default function Detail() {
-
-    // const { id } = useParams();
-    const blog = useLoaderData();
+  // const { id } = useParams();
+  const blog = useLoaderData();
 
   return (
-    <div className='mt-5 px-20'>
-        <div className="bg-gray-800 rounded-md p-5 my-5">
-            <b to="/" className="text-white text-xl hover:text-red-500"><strong>Title : {blog.title.toUpperCase()}</strong></b>
-            <p className="text-gray-300 text-[0.9rem] font-medium my-3">
-                {blog.subTitle}
-            </p>
-            <p className="text-gray-300 font-light mb-3">
-                {blog.content}
-            </p>
-        </div>
+    <div className="mt-5 px-20">
+      <div className="bg-gray-800 rounded-md p-5 my-5">
+        <b to="/" className="text-white text-xl hover:text-red-500">
+          <strong>Title : {blog.title.toUpperCase()}</strong>
+        </b>
+        <p className="text-gray-300 text-[0.9rem] font-medium my-3">{blog.subTitle}</p>
+        <p className="text-gray-300 font-light mb-3">{blog.content}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export const blogDetailLoader = async ( { params } ) => {
-    const { id } = params;
-    const res = await fetch('http://localhost:8000/blogs/' + id);
+export const blogDetailLoader = async ({ params }) => {
+  const { id } = params;
+  const res = await fetch("http://localhost:8000/blogs/" + id);
 
-    if (!res.ok) {
-        throw Error("blog not found.");
-    }
+  if (!res.ok) {
+    throw Error("blog not found.");
+  }
 
-    return res.json();
-}
+  return res.json();
+};
